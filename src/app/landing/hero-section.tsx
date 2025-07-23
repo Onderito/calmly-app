ee1ae41bf052e790d27e482e1864a02c9d2628e7
Onderito <ulas.onder@outlook.fr>
2025-07-23 15:13:02 +0200

    responsive for hero section done !

--- src/app/landing/hero-section.tsx
+++ src/app/landing/hero-section.tsx
@@ -5,18 +5,20 @@ import Button from "../components/Button";
 export default function HeroSection() {
   return (
     <section>
-      <h1 className="text-[#404040] font-manrope font-bold text-[38px] text-center leading-tight md:text-[64px] md:leading-18">
+      <h1 className="text-[#404040] font-manrope font-bold text-[38px] md:text-[64px] xl:text-[80px] text-center leading-tight  md:leading-18 xl:leading-20">
         Prenez soin de votre esprit, quelques minutes par jour.
       </h1>
-      <p className="text-[16px] text-[#404040] font-manrope text-center mt-6 md:text-[18px]">
+      <p className="text-[16px] text-[#404040] font-manrope text-center mt-6 xl:mt-8 md:text-[18px]">
         Un outil simple pour réduire le stress, suivre vos émotions et retrouver
         le calme.
       </p>
-      <div className="flex flex-col gap-4 mt-10">
-        <Button className="rounded-lg" variant="primary">
+      <div className="flex flex-col gap-4 mt-10 xl:flex-row xl:justify-center">
+        <Button className="cursor-pointer rounded-lg xl:p-2" variant="primary">
           Commencer gratuitement
         </Button>
-        <Button variant="secondary">Comment ça marche ?</Button>
+        <Button className="cursor-pointer" variant="secondary">
+          Comment ça marche ?
+        </Button>
       </div>
       <div className="flex justify-center items-end mt-10 relative">
         {/* Image gauche : avant */}
@@ -25,7 +27,7 @@ export default function HeroSection() {
             avant
           </p>
           <Image
-            className="rounded-3xl shadow-lg ring-4 ring-[#F3F3F3] -rotate-2 w-[150px] md:w-[180px]"
+            className="rounded-3xl shadow-lg ring-4 ring-[#F3F3F3] -rotate-2 w-[150px] md:w-[180px] xl:w-[200px] "
             src="/sad.webp"
             alt="sad"
             width={180}
@@ -39,7 +41,7 @@ export default function HeroSection() {
             après
           </p>
           <Image
-            className="rounded-3xl shadow-lg ring-4 ring-[#F3F3F3] rotate-2 w-[150px] md:w-[180px]"
+            className="rounded-3xl shadow-lg ring-4 ring-[#F3F3F3] rotate-2 w-[150px] md:w-[180px] xl:w-[200px]"
             src="/happy.webp"
             alt="happy"
             width={180}

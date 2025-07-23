860442625b8d9dfa099b1b3de88b9383cb459e0a
Onderito <ulas.onder@outlook.fr>
2025-07-23 14:51:12 +0200

    responsive for faq section done for tablet

--- src/app/landing/faq.tsx
+++ src/app/landing/faq.tsx
@@ -33,31 +33,35 @@ export default function Faq() {
   };
   return (
     <section>
-      <h2 className="text-center text-[#404040] text-[28px] font-bold leading-tight">
-        Questions fréquentes
+      <h2 className="text-center text-[#404040] text-[28px] md:text-[56px] font-manrope-bold leading-tight">
+        Questions{" "}
+        <span className="bg-gradient-to-r from-[#86C0FA] to-white text-transparent bg-clip-text">
+          fréquentes
+        </span>
       </h2>
-      <p className="text-center font-regular text-[14px] mt-4 text-[#404040]">
+      <p className="text-center font-regular text-[14px] md:text-[18px] mt-4 text-[#404040]">
         On répond en toute transparence à ce que vous vous demandez sûrement.
       </p>
       <div className="mt-10 flex flex-col gap-6">
         {faq.map((f, index) => (
           <div
             onClick={() => toggle(index)}
-            className="shadow-custom rounded-3xl flex flex-col gap-4 p-4"
+            className="shadow-custom rounded-3xl flex flex-col gap-4 p-4 cursor-pointer"
             key={index}
           >
-            <div className="flex gap-4 items-center">
+            <div className="flex gap-4 items-start ">
               <p>{f.img}</p>
-              <h3 className="font-manrope-bold text-[16px] text-[#404040]">
-                {" "}
-                {f.question}
-              </h3>
+              <div className="flex flex-col gap-2">
+                <h3 className="font-manrope-bold text-[16px] md:text-[18px] text-[#404040]">
+                  {f.question}
+                </h3>
+                {open === index && (
+                  <h4 className="font-manrope-regular text-[14px] md:text-[16px] text-[#404040]">
+                    {f.answer}
+                  </h4>
+                )}
+              </div>
             </div>
-            {open === index && (
-              <h4 className="font-manrope-regular text-[14px] text-[#404040]">
-                {f.answer}
-              </h4>
-            )}
           </div>
         ))}
       </div>

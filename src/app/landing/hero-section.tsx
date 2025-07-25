4e1851667a2e75d97ef5f30aa8ea5bc59b3d1148
Onderito <ulas.onder@outlook.fr>
2025-07-25 12:40:07 +0200

    add some animations, almost done

--- src/app/landing/hero-section.tsx
+++ src/app/landing/hero-section.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
 import Button from "../components/Button";
+import { easeInOut, motion } from "motion/react";
 
 export default function HeroSection() {
   return (
@@ -16,49 +19,115 @@ export default function HeroSection() {
           priority
         />
       </div>
-      <h1 className="text-[#404040] font-manrope font-bold text-[38px] md:text-[64px] xl:text-[80px] text-center leading-tight  md:leading-18 xl:leading-20">
+      <motion.h1
+        initial={{ opacity: 0, scale: 0.8, y: 20 }}
+        animate={{ opacity: 1, scale: 1, y: 0 }}
+        transition={{ duration: 1.2, ease: easeInOut }}
+        className="text-[#404040] font-manrope font-bold text-[38px] md:text-[64px] xl:text-[80px] text-center leading-tight  md:leading-18 xl:leading-20"
+      >
         Prenez soin de votre esprit, quelques minutes par jour.
-      </h1>
-      <p className="text-[16px] text-[#404040] font-manrope text-center mt-6 xl:mt-8 md:text-[18px]">
+      </motion.h1>
+      <motion.p
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1.2, ease: easeInOut, delay: 0.3 }}
+        className="text-[16px] text-[#404040] font-manrope text-center mt-6 xl:mt-8 md:text-[18px]"
+      >
         Un outil simple pour réduire le stress, suivre vos émotions et retrouver
         le calme.
-      </p>
-      <div className="flex flex-col gap-4 mt-10 xl:flex-row xl:justify-center">
+      </motion.p>
+      <motion.div
+        initial={{ opacity: 0, scale: 0.8, y: 20 }}
+        animate={{ opacity: 1, scale: 1, y: 0 }}
+        transition={{
+          type: "spring",
+          stiffness: 50,
+          damping: 14,
+          delay: 0.4,
+        }}
+        className="flex flex-col gap-4 mt-10 xl:flex-row xl:justify-center"
+      >
         <Button className=" rounded-lg xl:p-2" variant="primary">
           Commencer gratuitement
         </Button>
         <Button className="" variant="secondary">
           Comment ça marche ?
         </Button>
-      </div>
+      </motion.div>
       <div className="flex justify-center items-end mt-10 relative">
         {/* Image gauche : avant */}
-        <div className="z-0 relative">
-          <p className="font-gloria text-[12px] xl:text-[18px] text-[#404040] text-center mb-2">
+        <motion.div
+          whileHover={{ x: -5 }}
+          transition={{ duration: 0.5 }}
+          className="z-0 relative"
+        >
+          <motion.p
+            initial={{ opacity: 0, scale: 0.7, y: 40 }}
+            animate={{ opacity: 1, scale: 1, y: 0 }}
+            transition={{ duration: 1.2, ease: easeInOut, delay: 1.2 }}
+            className="font-gloria text-[12px] xl:text-[18px] text-[#404040] text-center mb-2"
+          >
             avant
-          </p>
-          <Image
+          </motion.p>
+          <motion.img
+            initial={{ opacity: 0, x: -100 }} // Départ : invisible et à gauche
+            animate={{
+              opacity: [0, 1, 1], // Keyframes pour l'opacité
+              x: [-100, 0, 0], // Keyframes pour la position X
+              y: [0, 50, 0], // Keyframes pour la position Y
+            }}
+            transition={{
+              duration: 2, // Durée totale de l'animation en secondes
+              times: [0, 0.5, 1], // Timing des keyframes :
+              // - 0 à 0.5 (50% du temps) : arrivée de la gauche
+              // - 0.5 à 1 (50% du temps) : montée
+              ease: "easeInOut",
+              delay: 0.4,
+            }}
             className="rounded-3xl shadow-lg ring-4 ring-[#F3F3F3] -rotate-2 w-[150px] md:w-[180px] xl:w-[200px] "
             src="/sad.webp"
             alt="sad"
             width={180}
             height={180}
           />
-        </div>
+        </motion.div>
 
         {/* Image droite : après, décalée vers le haut */}
-        <div className="z-10 relative -mt-6">
-          <p className="font-gloria text-[12px] xl:text-[18px] text-[#404040] text-center mb-2">
+        <motion.div
+          whileHover={{ x: 5 }}
+          transition={{ duration: 0.5 }}
+          className="z-10 relative -mt-6"
+        >
+          <motion.p
+            initial={{ opacity: 0, scale: 0.7, y: 40 }}
+            animate={{ opacity: 1, scale: 1, y: 0 }}
+            transition={{ duration: 1.2, ease: easeInOut, delay: 1.2 }}
+            className="font-gloria text-[12px] xl:text-[18px] text-[#404040] text-center mb-2"
+          >
             après
-          </p>
-          <Image
+          </motion.p>
+          <motion.img
+            initial={{ opacity: 0, x: -100 }} // Départ : invisible et à gauche
+            animate={{
+              opacity: [0, 1, 1], // Keyframes pour l'opacité
+              x: [100, 0, 0], // Keyframes pour la position X
+              y: [0, 50, 0], // Keyframes pour la position Y
+            }}
+            transition={{
+              duration: 2, // Durée totale de l'animation en secondes
+              times: [0, 0.5, 1], // Timing des keyframes :
+              // - 0 à 0.5 (50% du temps) : arrivée de la gauche
+              // - 0.5 à 1 (50% du temps) : montée
+              ease: "easeInOut",
+              delay: 0.4,
+            }}
             className="rounded-3xl shadow-lg ring-4 ring-[#F3F3F3] rotate-2 w-[150px] md:w-[180px] xl:w-[200px]"
             src="/happy.webp"
             alt="happy"
             width={180}
             height={180}
           />
-        </div>
+        </motion.div>
       </div>
     </section>
   );
--- src/app/landing/personal-moment.tsx
+++ src/app/landing/personal-moment.tsx
@@ -1,16 +1,33 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
 import Button from "../components/Button";
+import { easeInOut, motion } from "framer-motion";
 
 export default function PersonalMoment() {
+  const MotionButton = motion(Button);
+
   return (
     <section className="font-manrope relative ">
-      <h2 className="text-center text-[#404040] text-[28px] md:text-[56px]  font-bold leading-tight ">
+      <motion.h2
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, ease: "easeInOut" }}
+        viewport={{ once: true, amount: 0.4 }}
+        className="text-center text-[#404040] text-[28px] md:text-[56px]  font-bold leading-tight "
+      >
         Prenez un moment rien que{" "}
         <span className="text-[#B0B0B0] ">pour vous.</span>
-      </h2>
+      </motion.h2>
       <div className="content-container w-full flex flex-col xl:flex-row xl:items-center xl:gap-20 justify-center mt-8 xl:mt-12 ">
-        <div className="img-container relative xl:w-1/2 ">
+        <motion.div
+          initial={{ opacity: 0, x: -20, scale: 0.9 }}
+          whileInView={{ opacity: 1, x: 0, scale: 1 }}
+          transition={{ duration: 0.8, ease: "easeOut" }}
+          viewport={{ once: true, amount: 0.4 }}
+          className="img-container relative xl:w-1/2 "
+        >
           <Image
             className="rounded-2xl shadow-md shadow-[#86C0FA] md:w-full "
             src={"/happy-person.webp"}
@@ -28,43 +45,105 @@ export default function PersonalMoment() {
             &quot;Besoin de souffler&quot;
             <Image src={"/blow.svg"} alt="arrow" width={20} height={20} />
           </Button>
-        </div>
+        </motion.div>
 
-        <div className="mt-6 text-[#404040] xl:w-1/2">
-          <h3 className="font-manrope-bold text-[22px] md:text-[26px] xl:text-[32px] ">
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ staggerChildren: 0.2 }}
+          className="mt-6 text-[#404040] xl:w-1/2"
+        >
+          <motion.h3
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 },
+            }}
+            transition={{ duration: 0.8 }}
+            className="font-manrope-bold text-[22px] md:text-[26px] xl:text-[32px] "
+          >
             Exprimez ce que vous ressentez.
-          </h3>
-          <p className="font-regular text-[14px] md:text-[18px] xl:text-[20px] mt-2 text-[#404040]">
+          </motion.h3>
+          <motion.p
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 },
+            }}
+            transition={{ duration: 0.8 }}
+            className="font-regular text-[14px] md:text-[18px] xl:text-[20px] mt-2 text-[#404040]"
+          >
             Ecrivez librement, sans jugement. Un journal simple pour vous
             libérer l’esprit, en quelques mots.
-          </p>
-          <Button className="px-2 mt-8 rounded-lg" variant="primary">
+          </motion.p>
+          <MotionButton
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 },
+            }}
+            transition={{
+              duration: 0.8,
+              type: "spring",
+              stiffness: 120,
+              damping: 12,
+            }}
+            className="px-2 mt-8 rounded-lg"
+            variant="primary"
+          >
             Je commence maintenant
-          </Button>
-          <Button
-            className="relative left-40 md:left-80 md:top-[-80px] mt-6 flex gap-1 -rotate-12"
-            variant="noBlur"
+          </MotionButton>
+          <motion.div
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 },
+            }}
+            transition={{
+              duration: 0.8,
+              type: "spring",
+              stiffness: 60,
+              damping: 12,
+            }}
           >
-            &quot;Je vais y arriver&quot;
-            <Image src={"/happy.svg"} alt="arrow" width={20} height={20} />
-          </Button>
-          <Button
-            className="relative left-0 md:left-95 md:top-[-60px] mt-6 flex gap-1 rotate-2"
-            variant="noBlur"
-          >
-            &quot;Je suis fier de moi&quot;
-            <Image src={"/eyes-heart.svg"} alt="arrow" width={20} height={20} />
-          </Button>
-        </div>
+            <Button
+              className="relative left-40 md:left-80 md:top-[-80px] mt-6 flex gap-1 -rotate-12"
+              variant="noBlur"
+            >
+              &quot;Je vais y arriver&quot;
+              <Image src={"/happy.svg"} alt="arrow" width={20} height={20} />
+            </Button>
+            <Button
+              className="relative left-0 md:left-95 md:top-[-60px] mt-6 flex gap-1 rotate-2"
+              variant="noBlur"
+            >
+              &quot;Je suis fier de moi&quot;
+              <Image
+                src={"/eyes-heart.svg"}
+                alt="arrow"
+                width={20}
+                height={20}
+              />
+            </Button>
+          </motion.div>
+        </motion.div>
       </div>
-      <Image
-        className="absolute hidden top-5 right-[-300px] xl:block -z-10"
-        loading="lazy"
-        src={"/first-vector.svg"}
-        alt="line"
-        width={400}
-        height={100}
-      />
+
+      <svg
+        className="absolute hidden top-5 right-[-200px] xl:block -z-10"
+        width="311"
+        height="705"
+        viewBox="0 0 311 705"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <motion.path
+          initial={{ pathLength: 0 }}
+          whileInView={{ pathLength: 1 }}
+          transition={{ duration: 1.35, ease: easeInOut, delay: 0.9 }}
+          d="M13.2561 704C-9.47363 646 -9.19203 539.615 173.773 578.075C356.737 616.536 98.342 430.374 286.013 386.822C473.685 343.269 -5.06248 378.868 194.036 261.844C393.134 144.819 436.875 39.1877 433.857 1"
+          stroke="#86C0FA"
+          strokeOpacity="0.4"
+          strokeWidth="5"
+        />
+      </svg>
     </section>
   );
 }
